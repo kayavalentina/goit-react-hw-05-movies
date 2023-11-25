a0e79a2f89d10d5de4e26c198449fe38b6850b56
kayavalentina <131903622+kayavalentina@users.noreply.github.com>
2023-11-25 17:38:30 +0000

    Deploying to gh-pages from @ kayavalentina/goit-react-hw-05-movies@c4ef3309b61d0ed86625627bd293ed439cc65216 🚀

---
+++ static/js/884.18a8be06.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[884],{884:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});var n=r(184);function u(){return(0,n.jsx)("div",{children:"MovieDetails"})}}}]);
+//# sourceMappingURL=884.18a8be06.chunk.js.map
--- static/js/884.939a4f8d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[884],{647:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});var n=r(184);function u(){return(0,n.jsx)("div",{children:"MovieDetails"})}}}]);
-//# sourceMappingURL=884.939a4f8d.chunk.js.map
